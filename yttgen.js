6977aba6cbc913fb5107709ea57b0010d3887d55
Beej Jorgensen <beej@lambdaschool.com>
2018-06-19 17:28:07 -0700

    add image downloading

--- yttgen.js
+++ yttgen.js
@@ -28,12 +28,23 @@
 		ctx.drawImage(img, 0, 0);
 	}
 
+	function onDownloadClick() {
+		const aTag = this;
+		const canvas = qs('#canvas');
+
+		aTag.href = canvas.toDataURL('image/png');
+		//aTag.href = canvas.toDataURL('image/jpeg', 1.0); // 1.0 == max quality
+		aTag.download = 'ytbg.png';
+	}
+
 	function onImagesLoaded(imgs) {
 		drawImage(imgs[0]);
 
 		if (!isFontAvailable("Helvetica")) {
 			alert("Helvetica font not detected. Please make sure Helvetica is installed!");
 		}
+
+		qs('#download').addEventListener('click', onDownloadClick);
 	}
 
 	function onLoad() {
